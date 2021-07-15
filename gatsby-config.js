dc77573c43cb234dd714026bcc9303c50fc040a3
DeepaPrasanna <anneraj73@gmail.com>
2021-07-15 13:30:57 +0530

    favicons updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,7 +40,18 @@ module.exports = {
         background_color: `#141821`,
         theme_color: `#f6ad55`,
         display: `standalone`,
-        icons: [],
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
       },
     },
     `gatsby-plugin-offline`,
